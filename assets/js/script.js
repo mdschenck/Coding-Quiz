4d86c82150c72a1ae6baf062e0f2628b0b15bbea
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-03-27 01:38:51 -0400

    HTML & JS updates, got questions and answers added to page, and changing cards to show questions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 var timerElement = document.querySelector("#timerElement");
-var startButton = document.querySelector("#start-button");
+var startButton = document.querySelector("#startButton");
 var goBackButton = document.querySelector("#goBack");
 var clearHighScores = document.querySelector("#clearHighScores");
+var welcomeCard = document.querySelector("#welcomeCard");
+var questionCard = document.querySelector("#questions");
+var answers = document.querySelector("#answers");
+var answerWrongOrRight = document.querySelector("#answerWrongOrRight");
 
 var score = 0;
 let secondsLeft = 60;
@@ -18,29 +22,141 @@ function getScore() {
   }
 }
 
-function startGame() {
-  isWin = false;
-  timerCount = 10;
-  startButton.disabled = true; //disables start button
-  setTimer();
+function clearInterval() {
+  timerInterval = 0;
 }
 
-//NEED EVENT LISTENER for
+function startGame() {
+  //   startButton.disabled = true; //disables start button
+  setTimer();
+  renderQuestionOne();
+}
 
-// startButton.addEventListenter("click", startGame);
+function stopGame() {}
+
+function correctAnswer() {
+  // storedScore = storedScore + 20;
+  answerWrongOrRight.children[0].innerHTML = "<p>You Got It! Correct!</p>";
+  console.log("correct answer!");
+}
+
+function incorrectAnswer() {
+  answerWrongOrRight.children[0].innerHTML =
+    "<p>Ooh, bummer. That's Incorrect.</p>";
+  console.log("incorrect");
+}
+
+//    ** QUESTION ONE **
+
+function renderQuestionOne() {
+  questionCard.setAttribute("class", "show");
+
+  questionCard.children[0].children[0].textContent =
+    "What do horseys like to eat for din din";
+  questionCard.children[1].children[0].innerHTML = `<ul>
+<li><button id="1" class="btn1">green sludge</button></li>
+<li><button id="2" class="btn2">carbourators</button></li>
+<li><button id="3" class="btn3">old shoes</button></li>
+<li><button id="4" class="btn4">Apples!</button></li>
+</ul>
+`;
+
+  var btn1 = document.querySelector(".btn1");
+  var btn2 = document.querySelector(".btn2");
+  var btn3 = document.querySelector(".btn3");
+  var btn4 = document.querySelector(".btn4");
+
+  btn4.addEventListener("click", function () {
+    event.preventDefault();
+    correctAnswer();
+    renderQuestionTwo();
+  });
+
+  btn3.addEventListener("click", function () {
+    event.preventDefault();
+    incorrectAnswer();
+    renderQuestionTwo();
+  });
+
+  btn2.addEventListener("click", function () {
+    event.preventDefault();
+    incorrectAnswer();
+    renderQuestionTwo();
+  });
+
+  btn1.addEventListener("click", function () {
+    event.preventDefault();
+    incorrectAnswer();
+    renderQuestionTwo();
+  });
+
+  console.log(event);
+}
+
+// ** QUESTION TWO **
+
+function renderQuestionTwo() {
+  questionCard.children[0].children[0].textContent =
+    "What color is a bunny rabbert";
+  questionCard.children[1].children[0].innerHTML = `<ul>
+<li><button id="1" class="btn">purple</button></li>
+<li><button id="2" class="btn">The Sky</button></li>
+<li><button id="3" class="btn">bananas</button></li>
+<li><button id="4" class="btn">Such a cute light brown with grey spots and a pink nose.</button></li>
+</ul>
+`;
+  var btn1 = document.querySelector(".btn1");
+  var btn2 = document.querySelector(".btn2");
+  var btn3 = document.querySelector(".btn3");
+  var btn4 = document.querySelector(".btn4");
+
+  btn4.addEventListener("click", function () {
+    event.preventDefault();
+    correctAnswer();
+    renderQuestionThree();
+  });
+
+  btn3.addEventListener("click", function () {
+    event.preventDefault();
+    incorrectAnswer();
+    renderQuestionThree();
+  });
+
+  btn2.addEventListener("click", function () {
+    event.preventDefault();
+    incorrectAnswer();
+    renderQuestionThree();
+  });
+
+  btn1.addEventListener("click", function () {
+    event.preventDefault();
+    incorrectAnswer();
+    renderQuestionThree();
+  });
+
+  console.log(event);
+}
+
 // TIMER SECTION
 
 function setTimer() {
-  var timerInterval = setInterval(function () {
-    secondsLeft--;
-    timerElement.textContent = secondsLeft;
+  if (secondsLeft > 0) {
+    var timerInterval = setInterval(function () {
+      secondsLeft--;
+      timerElement.textContent = secondsLeft;
+    }, 1000);
 
-    if (secondsLeft === 0) {
+    if (secondsLeft == 0) {
       clearInterval(timerInterval);
+      stopGame();
+      return;
     }
-
+    return;
     console.log(secondsLeft + " Seconds Left!");
-  }, 1000);
+  }
 }
 
-setTimer();
+startButton = addEventListener("click", function () {
+  event.preventDefault();
+  startGame();
+});
